34286769467f53bf2ea04478fe625111c79ebfd3
solankiamn8 <solankiamn8@gmail.com>
2026-01-18 01:05:26 +0530

    chore(server): MongoDB connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
-const PORT = 8000
+const PORT = 8000;
+const startServer = async () => {
+    try {
+        await connectDB()
 
-app.listen(PORT, ()=>{
-    console.log("Server is running on port 8000")
-})
+        app.listen(PORT, () => {
+            console.log("Server is running on port 8000")
+        });
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+startServer()
