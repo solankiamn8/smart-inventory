34ba1aa54204f7219e3d7cbf7321c4ce43dc3478
solankiamn8 <solankiamn8@gmail.com>
2026-01-19 12:40:35 +0530

    feat(user): create user schema with encryption

---
+++ server/src/modules/users/users.schema.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["admin", "staff"],
+        default: "staff"
+    }
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    // don't rehash a hash
+    if (!this.isModified("password")) return next();
+
+    try {
+        // hash password after salt generation
+        this.password = await bcrypt.hash(this.password, 10);
+
+        // move on
+        next();
+    } catch (error) {
+        // error passed on to the controller
+        next(error);
+    }
+});
+
+export default mongoose.model("User", userSchema);
